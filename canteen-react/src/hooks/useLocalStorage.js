2dbf7d2854254e1e5ae0c4ea3d0bad5482ea6250
brynn1-png <bryanmagnaye2003@gmail.com>
2026-04-18 22:59:49 +0800

    deleting unused codes

--- canteen-react/src/hooks/useLocalStorage.js
+++
@@ -1,8 +0,0 @@
-import { useState } from 'react';
-
-const useLocalStorage = (key, initialValue) => {
-  const [value, setValue] = useState(initialValue);
-  return [value, setValue];
-};
-
-export default useLocalStorage;
